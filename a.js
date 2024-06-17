2ef92264461647457f2fdea2a182e96f4ce7386a
Rentschler <luizrentschler@gmail.com>
2024-06-17 11:24:30 -0300

    new code

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ /*let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let impares = [];
 let pares = [];
 
@@ -11,3 +11,21 @@ for (let i= 0; i < numeros.length; i++) {
 }
 numeros = pares.concat(impares);
 console.log(numeros)
+*/
+  
+
+ let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+ let pares = []
+ let impares = []
+ 
+ for (let i= 0; i < numeros.length; i++) {
+ if (numeros[i] % 2 == 0) {
+  pares.push(numeros[i])
+ } else {
+  impares.push(numeros[i])
+ }
+ }
+ let todos = [...pares, ...impares]
+ console.log(todos)
+ 
